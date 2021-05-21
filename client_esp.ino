538ea93efcb2cef45bd618521caa3a568fbf9a97
chrodream <chrodream.tj@gmail.com>
2021-05-21 15:21:10 +0900

    Enabled static IP address

--- client_esp.ino
+++ client_esp.ino
@@ -1,10 +1,16 @@
-#include "./src/WiFi.h"
-#include "./src/WiFiUdp.h"
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+// Config
 const char ssid[] = "WARPSTAR-A358DF"; // SSID
 const char pass[] = "9F60114A033FA";   // password
+static const int room_number = 205;32
+
+
 static WiFiUDP wifiUdp;
-static const char *raspberryIP = "192.168.0.100";
-static const int raspberryPort = 2001; //送信先のポート
+//ESP32 -> RPi(192.168.0.100 :2001)
+static const char *raspberryIP = "192.168.0.100"; // Raspberry Pi's IP
+static const int raspberryPort = 2001;            // Raspberry Pi's port
 
 static const int seconds = 1000;
 static const int minutes = 1000 * 60;
@@ -14,13 +20,14 @@ int port;
 
 static void WiFi_setup()
 {
-  static const int kLocalPort = 2000; //自身のポート
+  //RPi -> ESP32(DHCP:2002)
+  static const int esp32_port = 2002; //ESP's port
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
   }
-  wifiUdp.begin(kLocalPort);
+  wifiUdp.begin(esp32_port);
 }
 
 static void Serial_setup()
@@ -38,30 +45,30 @@ void setup()
 void loop()
 {
   int count = 0;
-  char i[64];
+  char rpi_input[32];
+  char key_stat[32];
 
   while (1)
   {
     count++;
 
-    //パケットの送信
+    //send packets
     wifiUdp.beginPacket(raspberryIP, raspberryPort);
-    wifiUdp.printf("ESP32Dev Borad: %d", count);
+    wifiUdp.printf("ESP32Dev Borad: %d", count); // send string
     wifiUdp.endPacket();
 
-    //パケットの受信
+    //recieve packets
     if (wifiUdp.parsePacket())
     {
-      wifiUdp.read(i, 64);
+      wifiUdp.read(rpi_input, 32);
       remoteIP = wifiUdp.remoteIP();
       port = wifiUdp.remotePort();
       Serial.print(remoteIP);
       Serial.print(" / ");
       Serial.print(port);
       Serial.print(" / ");
-      Serial.println(i); // UDP通信で来た値を表示
+      Serial.println(rpi_input); // UDP通信で来た値を表示
     }
-
-    delay(1 * minutes);
+    delay(1 * seconds);
   }
 }
